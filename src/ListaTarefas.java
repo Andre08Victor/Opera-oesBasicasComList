814b976b3c4c98aca4f05d19fb5f2cd7c2feb57a
Andre08Victor <avsm2411@gmail.com>
2024-06-06 09:23:30 -0300

    Ex Lista de tarefas utilizando lists e generics

--- src/ListaTarefas.java
+++ src/ListaTarefas.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefas {
-    
+    //atributo
+    private List<Tarefa> tarefaList;
+    //método construtor
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();//criação do ArrayList 
+    }
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();//.size representa a quantidade de elementos dentro de uma List
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
     
 }
