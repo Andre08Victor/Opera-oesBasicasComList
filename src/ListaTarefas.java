45b756f605fe16ff272347a9508d50f1f6728877
Andre08Victor <avsm2411@gmail.com>
2024-06-09 17:08:34 -0300

    Teste de classes e utilização da listas

--- src/ListaTarefas.java
+++ src/ListaTarefas.java
@@ -27,5 +27,21 @@ public class ListaTarefas {
         System.out.println(tarefaList);
     }
 
-    
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas(); //Criação do objeto
+        System.out.println("O número total de elementos na lista é " + listaTarefas.obterNumeroTotalTarefas());
+        
+        listaTarefas.adicionarTarefa("Tarefa 1");//Adicionando uma tarefa 
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        listaTarefas.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de elementos na lista é " + listaTarefas.obterNumeroTotalTarefas());
+        
+        listaTarefas.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.obterDescricoesTarefas();
+
+
+        
+    }
 }
